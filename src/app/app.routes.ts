b074982d7e9ea995a6a312abe8e636f46d6ddbe5
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-12 06:53:31 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
 import { ProductList } from './components/product-list/product-list';
 import { CartList} from './components/cart/cart';
-export const routes: Routes = [{ path: '', component: ProductList},
+export const routes: Routes = [{ path: 'p', component: ProductList},
   { path: 'cart', component: CartList}];
