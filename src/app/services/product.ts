6be6e71fcf253ec5540cc863052ef6dd6588474e
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-12 04:19:45 -0300

    Update product.ts

--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -2,14 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-export interface Product {
-  id: number;
-  name: string;
-  description: string;
-  price: number;
-  stock: number;
-  createdAt: string;
-}
 
 @Injectable({
   providedIn: 'root',
@@ -19,7 +11,8 @@ export class Product {
 
   constructor(private http: HttpClient) {}
 
-  getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.apiUrl, { withCredentials: true });
-  }
+  getProducts(): Observable<any[]> {
+  return this.http.get<any[]>(this.apiUrl);
+}
+
 }
