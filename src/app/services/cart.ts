fb0d5f294040f55fff8d2c1b4b157ed54388ed80
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-12 05:02:03 -0300

    Update cart.ts

--- src/app/services/cart.ts
+++ src/app/services/cart.ts
@@ -4,5 +4,23 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class Cart {
+  private items: any[] = [];
+
+  addToCart(product: any) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  getTotal() {
+    return this.items.reduce((sum, item) => sum + item.price, 0);
+  }
   
 }
