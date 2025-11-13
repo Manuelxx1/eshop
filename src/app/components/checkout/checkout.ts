2ae1bc9ff82e739b13c75762e574d81c145f92da
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-13 07:53:14 -0300

    Update checkout.ts

--- src/app/components/checkout/checkout.ts
+++ src/app/components/checkout/checkout.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './checkout.html',
   styleUrl: './checkout.css',
 })
-export class Checkout {
 
+
+  
+export class Checkout implements OnInit {
+  selectedProduct: any;
+  total: number = 0;
+
+  ngOnInit(): void {
+    const stored = localStorage.getItem('selectedProduct');
+    if (stored) {
+      this.selectedProduct = JSON.parse(stored);
+      this.total = this.selectedProduct.price;
+    }
+  }
+
+  finalizePurchase(): void {
+    alert('¡Compra realizada con éxito!');
+    localStorage.removeItem('selectedProduct');
+  }
 }
