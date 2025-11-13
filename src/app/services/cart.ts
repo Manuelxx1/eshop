d8442838f98b98d2444cca18e6f1b95f65cd6d92
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-13 02:28:07 -0300

    Update cart.ts

--- src/app/services/cart.ts
+++ src/app/services/cart.ts
@@ -5,18 +5,34 @@ import { Injectable } from '@angular/core';
 })
 export class Cart {
   private items: any[] = [];
+  constructor() {
+    const storedCart = localStorage.getItem('cart');
+    if (storedCart) {
+      this.items = JSON.parse(storedCart);
+    }
+  }
 
   addToCart(product: any) {
     this.items.push(product);
+    this.saveCart()
   }
 
   getItems() {
     return this.items;
   }
+  removeFromCart(productId: number): void {
+    this.items = this.items.filter(p => p.id !== productId);
+    this.saveCart();
+  }
 
   clearCart() {
     this.items = [];
-    return this.items;
+    this.saveCart();
+    
+  }
+
+  private saveCart(): void {
+    localStorage.setItem('cart', JSON.stringify(this.cart));
   }
 
   getTotal() {
