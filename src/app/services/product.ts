7d73f768f7c5ca2f7c72ae3b20e6286dd13e4a3a
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-15 05:53:35 -0300

    Update product.ts

--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -2,19 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+export interface Order {
+  id: number;
+  productName: string;
+  amount: number;
+  status: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class Product {
   private apiUrl = 'https://portfoliowebbackendkoyeb-1.onrender.com/api/products/search';
-
+private apiUrlOrders = 'https://portfoliowebbackendkoyeb-1.onrender.com/api/payments';
   constructor(private http: HttpClient) {}
 
   // Método para buscar productos por término
   searchProducts(term: string): Observable<any[]> {
   return this.http.get<any[]>(`${this.apiUrl}?name=${term}`);
 }
+
+
+  //para consultar el estado del pedido orders
+  //que se registro en tabla orders
+  getOrder(id: number): Observable<Order> {
+    return this.http.get<Order>(`${apiUrlOrders}/orders/${id}`);
+  }
 
 
 }
