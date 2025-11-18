e7f2ded53110e5638e563c2ddfc6f6df71e4cba1
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-18 01:40:26 -0300

    Update product.ts

--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -24,13 +24,13 @@ private apiUrlOrders = 'https://portfoliowebbackendkoyeb-1.onrender.com/api/paym
 
   // Método para crear la preferencia y devolver el orderId
   comprar(product: any): Observable<string> {
-    alert("Recibido en el service: " + product.name);
   return this.http.post(
     'https://portfoliowebbackendkoyeb-1.onrender.com/api/payments/create',
     product,
-    { responseType: 'text' } // esto evita el error de parsing 
-    //ya que el backend envía un texto plano
+    { responseType: 'text' } //  esto evita el error de parsing
   );
+}
+
 
 
   //para consultar el estado del pedido orders
