370f4d890b1b0c429fa4101ef33f21e495578d73
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-26 05:38:04 -0300

    Refactor cart service methods for clarity and consistency

--- src/app/components/cart/cart.ts
+++ src/app/components/cart/cart.ts
@@ -22,18 +22,12 @@ export class CartList implements OnInit{
   this.items = this.cartService.getItems();
   this.total = this.cartService.getTotal();
   */
-    this.cartService.getItems().subscribe(data => {
-    this.items = data;
-    this.total = this.items.reduce((sum, item) => sum + item.product.price * item.quantity, 0);
-  });
+    this.cartService.getItems().subscribe(items => {
+      this.items = items;
+      this.total = this.items.reduce((sum, i) => sum + i.product.price * i.quantity, 0);
+    });
 }
 
-clearCart() {
-  this.cartService.clearCart().subscribe(() => {
-    this.items = [];
-    this.total = 0;
-  });
-}
 
 //métodos para agregar o quitar items 
   //directamente desde el carrito localStorage 
@@ -52,25 +46,35 @@ decrease(productId: number) {
 */
 
   //version backend 
-increase(productId: number) {
-  this.cartService.addToCart(productId, 1).subscribe(() => {
-    // Una vez que el backend confirma, recargamos el carrito
-    this.cartService.getItems().subscribe(items => {
-      this.items = items;
-      this.total = this.items.reduce((sum, i) => sum + i.product.price * i.quantity, 0);
+add(productId: number) {
+    this.cartService.addToCart(productId, 1).subscribe(() => {
+      
     });
-  });
-}
+  }
 
-  //para quitar items del carrito 
-decrease(productId: number) {
-  this.cartService.decreaseFromCart(productId).subscribe(() => {
-    this.cartService.getItems().subscribe(items => {
-      this.items = items;
-      this.total = this.items.reduce((sum, i) => sum + i.product.price * i.quantity, 0);
+  increase(productId: number) {
+    this.cartService.increaseFromCart(productId).subscribe(() => {
+      
     });
-  });
-}
+  }
+
+  decrease(productId: number) {
+    this.cartService.decreaseFromCart(productId).subscribe(() => {
+      
+    });
+  }
+
+  remove(cartItemId: number) {
+    this.cartService.removeFromCart(cartItemId).subscribe(() => {
+      
+    });
+  }
+
+  clear() {
+    this.cartService.clearCart().subscribe(() => {
+      
+    });
+  }
 
 
 
