301d06e7ac0ed384a66a31d941a91fa83059837f
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-11-29 04:23:08 -0300

    Update cart.ts

--- src/app/services/cart.ts
+++ src/app/services/cart.ts
@@ -102,7 +102,7 @@ getItems() {
 
   // Método para comprar el carrito
   comprarCarrito(cartItems: any[]): Observable<string> {
-    return this.http.post<string>(`${this.apiUrl}/api/payments/create-cart`, cartItems);
+    return this.http.post<string>(`${this.apiUrl}/api/payments/create-cart`, cartItems, { responseType: 'text' });
   }
 
 
