99d86f3787f008d72bb01a021734e3c21dce3843
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-04 06:46:30 -0300

    Update registrarse.ts

--- src/app/components/registrarse/registrarse.ts
+++ src/app/components/registrarse/registrarse.ts
@@ -81,7 +81,7 @@ constructor(private miServicio: Product,private fb: FormBuilder,private cdRef: C
   formularioregistrodatos() {
   if (this.formularioregistro.valid) {
     console.log('Payload:', this.formularioregistro.value); // para depurar
-    alert(this.formularioregistro.value);
+    alert(this.formularioregistro.value.username);
     //this.formularioregistro.value envía todo el objeto ocsea tidos los campos 
     //a registrarDatos(user: any): Observable<any>
     this.miServicio.registrarDatos(this.formularioregistro.value).subscribe({
