6cd280122bc465dc7c30a5a5388f06d760d36333
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-05 04:33:21 -0300

    Update product.ts

--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -81,10 +81,9 @@ registrarDatos(user: any): Observable<any> {
     
   // Método para crear la preferencia y devolver el orderId
 // Método para crear la preferencia y devolver el initPoint
-comprar(productId: number, quantity: number, usuario: string | null): Observable<any> {
+comprar(productId: number, quantity: number, usuario: string | null): Observable<string> {
   const body = { quantity, usuario };
-  return this.http.post(
-    `https://portfoliowebbackendkoyeb-1-ulka.onrender.com/api/payments/create/${productId}`,
+  return this.http.post(`https://portfoliowebbackendkoyeb-1-ulka.onrender.com/api/payments/create/${productId}`,
     body,
     { responseType: 'text' }
   );
