915bb41e2d4deb0d14f07fe330c1eac50720105b
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-06 05:45:40 -0300

    Update product-list.ts

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -157,7 +157,7 @@ if (this.formulariologin.valid) {
         this.datosDebug += `\nRespuesta: ${JSON.stringify(res)}`;
    
         //  Guardar sesión en localStorage
-        localStorage.setItem('usuario', res.loginUsername);
+        localStorage.setItem('usuario', res.Username);
 
 this.session();
         
