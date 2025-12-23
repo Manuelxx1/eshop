be06cd89228a76be8d6f9e57eb583d3a454dbf18
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-23 06:09:34 -0300

    Update product-list.ts

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -197,15 +197,13 @@ this.resetTimer();
 this.fechaderegistro = localStorage.getItem('createdAt');
 
 
-// Usamos el método público del servicio
-      this.productService.subscribeToNotifications((msg) => {
-        this.notifications.push(msg);
-      });
-        // Activar la conexión si aún no está activa
-        if (!this.productService.stompClient.active) {
-          this.productService.stompClient.activate();
-        }
-      
+this.productService.stompClient.onConnect = () => { 
+  this.productService.stompClient.subscribe('/topic/notificaciones', (message) => {
+    this.notifications.push(message.body); }); 
+};
+      if (!this.productService.stompClient.active) { 
+        this.productService.stompClient.activate(); 
+      }
 
       
     }//ngOnInit 
