fc63b333cf4a3a2a3ae29d9cf29c8458d8205165
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-24 05:21:20 -0300

    Update ws-test-component.ts

--- src/app/components/ws-test-component/ws-test-component.ts
+++ src/app/components/ws-test-component/ws-test-component.ts
@@ -10,19 +10,19 @@ import { Product } from '../../services/product';
 export class WsTestComponent implements OnInit {
   conexionActiva = false; 
   notifications: string[] = [];
-
   constructor(private productService: Product) {}
-  ngOnInit(): void {
-    this.productService.stompClient.onConnect = () => {
-      this.conexionActiva = true;
-      this.productService.stompClient.subscribe('/topic/notificaciones', (message) => {
+  ngOnInit(): void { 
+    this.productService.stompClient.onConnect = () => { 
+      this.conexionActiva = true; 
+      this.productService.stompClient.subscribe('/topic/notificaciones', (message) => { 
         this.notifications.push(message.body); 
-      });
-    }; 
-    this.productService.stompClient.activate(); 
+      }); 
+    };
+    this.productService.stompClient.activate();
   } 
-  sendTestNotification(): void {
-    this.productService.sendNotification('Hola desde Angular ');
-  }                                                                                                                                                                                                                                                                                                                                                                 }
+  sendTestNotification(): void { 
+    this.productService.sendNotification('Hola desde Angular 🚀');
+  }
 
+  
 }
