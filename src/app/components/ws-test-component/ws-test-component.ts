c820b92f50e4fffafe2ece2485273b17d6c62d38
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-24 04:56:37 -0300

    Update ws-test-component.ts

--- src/app/components/ws-test-component/ws-test-component.ts
+++ src/app/components/ws-test-component/ws-test-component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from './product.service';
+import { Product } from '../../services/product';
 
 @Component({
   selector: 'app-ws-test-component',
