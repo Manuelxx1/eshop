d6edb252ef243d3a6382fbdc72adda524f2dcea1
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-27 06:00:31 -0300

    Add CommonModule and WsTestComponent to imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
+import { WsTestComponent } from './components/ws-test-component/ws-test-component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterModule],
+  imports: [RouterOutlet,RouterModule, CommonModule, WsTestComponent  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('eshop');
+  title = 'Mi App Angular con WebSocket';
 }
