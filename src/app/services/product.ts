1b5f028767c21659502a7646dc25b86ff6390167
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2025-12-27 02:37:15 -0300

    Update product.ts

--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -76,10 +76,7 @@ private apiUrl = 'https://portfoliowebbackendkoyeb-1-ulka.onrender.com/api/produ
     //notificaciones mediante websocket/stomp 
 
 
-this.stompClient = new Client({ webSocketFactory: () => new SockJS('https://portfoliowebbackendkoyeb-1-ulka.onrender.com/ws'),
-                               reconnectDelay: 5000, 
-                              //debug: (str) => console.log(str), 
-                              });
+this.stompClient = new Client({ webSocketFactory: () => new SockJS('https://portfoliowebbackendkoyeb-1-ulka.onrender.com/ws'), reconnectDelay: 5000, debug: (str) => console.log(str), });
   
   }//constructor
 
