7aaf6241c1027cb91234104a3d7e16417f5cfe2f
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-01-09 07:25:47 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { ProductList } from './components/product-list/product-list';
 import { CartList} from './components/cart/cart';
 import { Checkout} from './components/checkout/checkout';
+import { CompraExitosa } from './components/compra-exitosa/compra-exitosa';
 import { Registrarse } from './components/registrarse/registrarse';
 export const routes: Routes = [{ path: '', component: ProductList},
   { path: 'cart', component: CartList},
      { path: 'test-checkout', component: Checkout },
                           
 { path: 'comprar', component: Checkout },
+ { path: 'compra-exitosa', component: CompraExitosa },
 { path: 'registrarse', component: Registrarse }
 ];
