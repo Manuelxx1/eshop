7ea2183600bbe3185d003d30d0a8a850b7a3c102
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-01-09 07:04:02 -0300

    Update compra-exitosa.ts

--- src/app/components/compra-exitosa/compra-exitosa.ts
+++ src/app/components/compra-exitosa/compra-exitosa.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-compra-exitosa',
   imports: [],
   templateUrl: './compra-exitosa.html',
   styleUrl: './compra-exitosa.css',
 })
-export class CompraExitosa {
-
+export class CompraExitosa implements OnInit {
+  status: string | null = null; 
+  paymentId: string | null = null;
+  externalReference: string | null = null; 
+  constructor(private route: ActivatedRoute) {} 
+ 
+  ngOnInit(): void { 
+    this.route.queryParamMap.subscribe(params => { 
+      this.status = params.get('status');
+      this.paymentId = params.get('payment_id'); 
+      this.externalReference = params.get('external_reference'); 
+    }); 
+  }
 }
+
+
