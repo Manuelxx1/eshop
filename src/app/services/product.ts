fa92d363941cb60c6cfcc1134d6ad7dc4866be13
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-01-13 05:05:47 -0300

    Update product.ts

--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -41,6 +41,7 @@ export interface Order {
   createdAt: string;
   loginUsername:String;
   externalReference: string; // <-- NUEVO campo compra exitosa   items: OrderItem[];
+items: OrderItem[];
 }
 
 @Injectable({
