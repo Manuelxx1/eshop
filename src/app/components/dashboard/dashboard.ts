e916844864abcd56ba00c5db995c54480f0f0bd1
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-03-17 05:13:59 -0300

    Update dashboard.ts

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
