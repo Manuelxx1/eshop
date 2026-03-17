3c59952bcb0297a731794c01a426eb8dcaf91daa
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-03-17 05:11:44 -0300

    Update dashboard.ts

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   imports: [],
@@ -7,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+
+  sesionActivaSinGoogle = false;
+  datosdesesion: string = "";
+
+  constructor(private router: Router ) {
+    
+  }
+
+  ngOnInit(): void {
+    this.session(); //  se ejecuta al cargar el dashboard
+  }
+
+  session() {
+    const usuarioGuardado = localStorage.getItem('usuario');
+    const id = localStorage.getItem('idUsuario');
+    if (usuarioGuardado) {
+      this.sesionActivaSinGoogle = true;
+      this.datosdesesion = usuarioGuardado;
+      this.idUsuario = id;
+    } else {
+      this.sesionActivaSinGoogle = false;
+      this.datosdesesion = "";
+      
+    }
+  }
+
+  cerrarSesion() {
+    localStorage.clear();
+    this.sesionActivaSinGoogle = false;
+    this.datosdesesion = "";
+    
+    // Redirigir al inicio o login
+    this.router.navigate(['/']);
+  }
 
 }
