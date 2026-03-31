ab1a1396045bb6ab779539126bcc33e00fa960e2
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-03-31 05:59:18 -0300

    Implement product search with reactive forms
    Added reactive form controls for product search and implemented search functionality.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
-
+import { FormControl,ReactiveFormsModule, FormBuilder,FormGroup,Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { WsTestComponent } from './components/ws-test-component/ws-test-component';
+import { Product,Order} from '../../services/product';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,RouterModule, CommonModule, WsTestComponent  ],
@@ -11,6 +12,45 @@ import { WsTestComponent } from './components/ws-test-component/ws-test-componen
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('eshop con WebSocket');
+  //title se nuestra en el FrontEnd así {{title}}
+  //protected readonly title = signal('eshop con WebSocket');
+//recibir datos del formulario buscador
+  searchControl = new FormControl('');
+  menuOpen = false;
+
+  products: any[] = [];
+
   
+  constructor(private productService: Product ){}
+  ngOnInit(): void {
+      
+          this.searchControl.valueChanges.subscribe(term => {
+      const query = term?.trim();
+      if (query && query.length >= 2) {
+        this.loading = true;
+        this.productService.searchProducts(query).subscribe({
+          next: data => {
+            this.products = data;
+            this.loading = false;
+            this.error = false;
+          },
+          error: err => {
+            console.error('Error al buscar productos', err);
+            this.products = [];
+            this.loading = false;
+            this.error = true;
+          }
+        });
+      } else {
+        this.products = [];
+      }
+    });
+
+    }//ngOnInit 
+
+  //para Mostrar el menú en movil
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
 }
