ce3e58f84ab13045301d939ee6287285defeddac
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-04-01 08:27:03 -0300

    Update categoria.ts

--- src/app/components/categoria/categoria.ts
+++ src/app/components/categoria/categoria.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { TitleCasePipe } from '@angular/common';
+import { TitleCasePipe,CurrencyPipe} from '@angular/common';
 import { Product} from '../../services/product';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-categoria',
-  imports: [TitleCasePipe,CommonModule],
+  imports: [TitleCasePipe,CommonModule,CurrencyPipe],
   templateUrl: './categoria.html',
   styleUrls: ['./categoria.css']
 })
@@ -20,5 +20,11 @@ export class Categoria implements OnInit {
   ngOnInit() {
     this.categoriaNombre = this.route.snapshot.paramMap.get('nombre') || '';
   this.productosporcategoria = this.productService.getProductsByCategory(this.categoriaNombre);
+  }
+
+  //este método es solo de ejemplo
+  //colocar aqui el que esta en product-list.ts
+  addToCart(product: any) {
+    console.log('Producto agregado al carrito:', product);
   }
 }
