0cb39d846e700909be9fe5fe6132c45404fcd395
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-04-03 09:21:24 -0300

    Update product-list.ts

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -61,9 +61,7 @@ export class ProductList  {
 total: number = 0;
 
 
-//para el login
-  formulariologin: FormGroup;
-  sesionActivaSinGoogle = false;
+
   datosDebug: string = '';
   email:string = '';  
   fechaderegistro:any;
@@ -90,11 +88,7 @@ initPointUrl: string | null = null;
   
 
 
-//2FA validar 
-  
-  twofaForm: FormGroup;
-  step = 1;
-  message = '';
+
 
   checkoutForm: FormGroup;
   //opciones de envío
@@ -131,10 +125,7 @@ this.checkoutForm = this.fb.group({
 });
 
 
-    //Recibir Código 2FA
-    this.twofaForm = this.fb.group({
-      code: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(6)]]
-    });
+    
     
   }// constructor 
 
@@ -167,14 +158,9 @@ goBack() {
   }
 }
 
-     /*
-  //para Mostrar el menú en movil
-  toggleMenu() {
-    this.menuOpen = !this.menuOpen;
-  }
-  */
+
   
-  /*
+  
   ngOnInit(): void {
       
           this.searchControl.valueChanges.subscribe(term => {
@@ -199,13 +185,12 @@ goBack() {
       }
     });
 
-    }//ngOnInit 
-    */
-
-  ngOnInit() {
     this.featuredProducts = this.productService.getFeaturedProducts();
+
+    }//ngOnInit 
     
-  }
+
+  
 
 
       
@@ -220,107 +205,7 @@ goBack() {
   
 
 
-      formulariologindatos() {
-if (this.formulariologin.valid) {
-
-  const nombre = this.formulariologin.value.username;
-    const password = this.formulariologin.value.password;
-
-    //this.datosDebug = `Enviando: ${nombre} / ${password}`;
       
-  
-  this.productService.iniciarSesion(this.formulariologin.value.username,this.formulariologin.value.password).subscribe({
-      next: res => {
-        if (res.status === 202) {
-      // Mostrar formulario de ingreso de código 2FA
-      console.log("Login pendiente de 2FA:", res.body.mensaje);
-         // Si usás observe: 'response'
-          // res.body es un objeto con varias propiedades (id, usuario, mensaje, etc.).
-//entonces se accede asi
-          alert("Credenciales de sesion correctas Login pendiente de 2FA:" + res.body.mensaje);
-   // Si NO usás observe: 'response'
-         // En ese caso, Angular te devuelve directamente el body (el JSON)
-          //Ahí deberías hacer simplemente:
-         // alert("Credenciales de sesion correctas Login pendiente de 2FA:" + res.mensaje);
-         this.email = res.body.email;
-          this.step = 2;
-          
-          } 
-        
-  },
-  error: err => {
-    // Login fallido
-    if (err.status === 401) {
-    console.error('Error de login:', err);
-this.message = 'Credenciales inválidas';
-    //this.datosDebug += `\nError: ${JSON.stringify(err)}`;
-    alert('Nombre o contraseña incorrectos');
-  }
-  }
-});
-  } else {
-    alert('Por favor completá todos los campos');
-  }
-      
-    }//formulariologin 
-
-
-  //Validar 2FA
-    onValidateCode() {
-  const code = this.twofaForm.value.code;
-  alert("Validando con email:"+this.email+"y código:"+code);
-
-  this.productService.validateCode(this.email, code).subscribe({
-    next: (res) => {
-      
-      if (res.status === 200) {
-        alert("Bienvenido " + res.body.name + " - " + res.body.mensaje);
-        // Login completo
-     // console.log("Login exitoso:", res.body);
-          //alert("Login exitoso:" + res.body);
-      
-    
-        
-//agregar los datos de la response a la property 
-       // this.datosDebug += `\nRespuesta: ${JSON.stringify(res)}`;
-   
-        // Guardar sesión en localStorage usando res.body
-    localStorage.setItem('idUsuario', res.body.id);
-    localStorage.setItem('usuario', res.body.usuario);
-    localStorage.setItem('email', res.body.email);
-    localStorage.setItem('name', res.body.name);
-    localStorage.setItem('createdAt', res.body.createdAt);
-
-    this.nombre = res.body.name;
-    this.email = res.body.email;
-    this.fechaderegistro = res.body.createdAt;
-    this.message = res.body.mensaje;
-
-    
-        
-//this.cargarDatosDashboard(res.usuario);
-     
-        
-        
-        
-        alert(res.body.id); //mensaje del.backend por ejemplo: "Login exitoso"
-        
-        this.router.navigate(['/dashboard']);
-        
-      }
-    },
-    error: (err) => {
-      console.error("Error backend:", err);
-      this.message = err.error?.error || "Error de seguridad";
-      alert ("Error en la validación"+err);
-      alert ("Error en la validación"+err.error);
-      alert ("Error en la validación"+err.error?.error || "Error de seguridad");
-    alert ("Error en la validación"+err.body.error);
-    }
-    
-  });
-}
-
 
 
 
