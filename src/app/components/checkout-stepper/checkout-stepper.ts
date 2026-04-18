73b2237ebed1d15108a075826ae636d59d8ea9b8
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-04-18 10:39:59 -0300

    Update checkout-stepper.ts

--- src/app/components/checkout-stepper/checkout-stepper.ts
+++ src/app/components/checkout-stepper/checkout-stepper.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input} from '@angular/core';
 import { FormControl,ReactiveFormsModule, FormBuilder,FormGroup,Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
