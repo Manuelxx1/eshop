9696cf4ce5ca50061b66491a9f32b40df249d44d
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-04-28 08:25:42 -0300

    Add CheckoutStepper to Todoslosproductos component

--- src/app/components/todoslosproductos/todoslosproductos.ts
+++ src/app/components/todoslosproductos/todoslosproductos.ts
@@ -1,13 +1,17 @@
 import { Component,OnInit } from '@angular/core';
-
+import { Product,Order} from '../../services/product';
 import { RouterModule } from '@angular/router';
 import { FormControl,ReactiveFormsModule, FormBuilder,FormGroup,Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Product,Order} from '../../services/product';
+import { CheckoutStepper} from '../../components/checkout-stepper/checkout-stepper';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-todoslosproductos',
-  imports: [RouterModule, CommonModule,ReactiveFormsModule],
+  imports: [RouterModule, CommonModule,ReactiveFormsModule,CheckoutStepper],
   templateUrl: './todoslosproductos.html',
   styleUrl: './todoslosproductos.css'
 })
@@ -94,6 +98,38 @@ const valorId = localStorage.getItem('idUsuario');
   this.productService.clearPendingCheckout();
   alert('Clave borrada al cerrar modal');
   }
+
+
+  addToCart(product: any): void {
+  const quantity = this.quantityControl.value; //  acá tomás el valor del select
+  const idUsuario = Number(localStorage.getItem('idUsuario'));
+if (idUsuario) {
+    // Usuario logueado → carrito en backend 
+    this.cartService.addToCart(product.id, quantity,idUsuario ).subscribe({
+    next: (cart) =>{
+      console.log("Carrito actualizado en backend:", cart);
+      // No hace falta asignar nada manualmente si tu servicio ya hace tap() y actualiza itemsSubject
+    // La vista se refresca sola porque el HTML usa cartService.items$ | async
+    },
+    error: err =>{
+      console.error('Error al agregar al carrito', err);
+    }
+  });
+      } else {
+    // Usuario sin sesión → carrito local
+  //en este caso se pasa el objeto product completo porque la interfaz   
+  //CartItem lo requeria por esta definición private items: CartItem[] = [];
+  //si definimos así private items: Any [] = []; no iba a ser necesario obedecer 
+  //a las propiedades declaradas en la interfaz CartItem 
+  //y se le podria pasar cualquier tipo de datos a nuestro método addItem
+  //eso facilita el código pero en producción se usa la interfaz 
+  //porque  te facilita mostrar nombre, precio, imagen, etc. en la página del carrito sin errores.
+  this.cartService.addItem( product, quantity );
+    console.log("Agregado al carrito local");
+}
+}
+
+
 
   
 }
