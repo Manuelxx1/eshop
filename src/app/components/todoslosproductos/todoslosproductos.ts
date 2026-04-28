f50b782a0e6b956c614e552df18b40bc957a626c
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-04-28 08:17:32 -0300

    Implement pending checkout handling and modal logic
    Added logic to handle pending checkout and modal display.

--- src/app/components/todoslosproductos/todoslosproductos.ts
+++ src/app/components/todoslosproductos/todoslosproductos.ts
@@ -18,7 +18,8 @@ export class Todoslosproductos implements OnInit {
   loading = true;
   error = false;
   
-
+selectedProduct: any;
+  showStepperModal = false;
   
   constructor(private productService: Product ){}
 
@@ -39,9 +40,60 @@ ngOnInit(): void {
         this.error = true;
       }
     });
-      }
+
+        const pendingCheckout = this.productService.getPendingCheckout();
+alert('PendingId leído en ProductComponent:'+ pendingCheckout?.productId);
+
+if (pendingCheckout && this.isLoggedIn()) {
+  const product = this.products.find(p => p.id === pendingCheckout.productId);
+  alert('Producto encontrado:' + product?.name);
+  
   
 
+if (product) {
+  this.selectedProduct = selected;
+  this.showStepperModal = true;
+  
+
+    //para que no quede en localStorage luego de finalizar la compra
+       //esto evita que el stepper vuelva a aparecer sin que lo llamaramos
+        //y nos permita otra vez buscar algún otro producto que deseamos comprar 
+        
+    localStorage.removeItem('pendingCheckout');
+}
+  }
+      }
+
+  //mostrar el boton compra directa que abre el modal stepper solo si hay session
+  isLoggedIn(): boolean {
+  const valorId = localStorage.getItem('idUsuario');
+  return !!valorId; // true si hay sesión
+  }
+
+  goToLogin(type: 'product' | 'featured' | 'category' | 'offers',productId: number ) {
+  this.productService.setPendingCheckout(type, productId);
+  this.router.navigate(['/login']);
+  }
+
+  
+  
+// Caso 1: usuario ya logueado
+  startCheckout(product: any) {
+const valorId = localStorage.getItem('idUsuario');
+  const idUsuario = valorId ? Number(valorId) : null; //  conversión a número
+    if(idUsuario){
+  this.selectedProduct = product;
+  this.showStepperModal = true;   
+    }
+  }
+
+  closeStepper() {
+  this.showStepperModal = false;    // cierra el modal
+  this.selectedProduct = null; // limpia selección si querés
+ // limpiar la clave recién al cerrar el modal
+  this.productService.clearPendingCheckout();
+  alert('Clave borrada al cerrar modal');
+  }
 
   
 }
