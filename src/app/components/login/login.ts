9a0ba7647a1b0cf8395425cec50e846ed3c5bce3
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-04-29 11:43:56 -0300

    Handle pending checkout redirection in login
    Add alert and navigation for pending checkout scenarios

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -162,6 +162,10 @@ const pendingCheckoutCategory = this.productService.getPendingCheckoutCategory()
     // Si había un carrito pendiente → redirige al carrito
     alert("datos del pendingCheckout en login para menu "+pendingCheckout.productId);
     this.router.navigate(['/productos']);
+  }else if (pendingCheckout?.type==="allproductsearch") {
+    // Si había un carrito pendiente → redirige al carrito
+    alert("datos del pendingCheckout en login para el buscador principal"+pendingCheckout.productId);
+    this.router.navigate(['/productos']);
   
     // Última intención: producto puntual
     //tiene prioridad sobre un carrito guardado anteriormente
