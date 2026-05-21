7cd93fb5efbe02a29df5b5028b9f1d20c09dd87b
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-05-21 05:33:13 -0300

    Add Reactive Forms and reset password functionality

--- src/app/components/forgot-password/forgot-password.ts
+++ src/app/components/forgot-password/forgot-password.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet,RouterLink } from '@angular/router';
+import { FormControl,ReactiveFormsModule, FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule,RouterLink],
   templateUrl: './forgot-password.html',
   styleUrl: './forgot-password.css',
 })
 export class ForgotPassword {
+
+  constructor(private loginService: LoginService, private cartService: Cart,private router: Router,private fb: FormBuilder,private route: ActivatedRoute) {
+
+forgotPasswordForm = this.fb.group({
+  email: ['', [Validators.required, Validators.email]]
+});
+  }
+
+onSubmit() {
+  this.loginService.sendPasswordReset(this.forgotPasswordForm.value.email)
+    .subscribe(() => alert('Se envió un enlace de recuperación a su correo'));
+}
+
+
 
 }
