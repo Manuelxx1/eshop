2443b2b9ccfb0f1717db4b2c1431aba8967a3d4c
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-05-22 11:16:01 -0300

    Update reset-password.ts

--- src/app/components/reset-password/reset-password.ts
+++ src/app/components/reset-password/reset-password.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule,Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../services/product';
 
