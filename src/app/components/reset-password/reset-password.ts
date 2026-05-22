b9c04e88f2554c6aa43ebe8a203b20b2736ee91d
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-05-22 08:01:45 -0300

    Add reset password form and logic
    Implement reset password functionality with form handling.

--- src/app/components/reset-password/reset-password.ts
+++ src/app/components/reset-password/reset-password.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../services/product';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrl: './reset-password.css',
 })
 export class ResetPassword {
+
+  resetForm!: FormGroup;
+  token!: string;
+
+  constructor(
+    private fb: FormBuilder, private route: ActivatedRoute, private productService: Product) {}
+
+  ngOnInit(): void {
+    // Leer el token de la URL
+    this.token = this.route.snapshot.queryParamMap.get('token') || '';
+
+    // Crear el formulario
+    this.resetForm = this.fb.group({
+      newPassword: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.resetForm.valid) {
+      const newPassword = this.resetForm.value.newPassword;
+      this.productService.resetPassword(this.token, newPassword).subscribe({
+        next: () => alert('Contraseña actualizada correctamente'),
+        error: (err) => alert('Error al actualizar contraseña: ' + err.message)
+      });
+    }
+  }
 
 }
