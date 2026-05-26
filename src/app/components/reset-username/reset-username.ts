c86eb16a82dc5fbcc32ee3e48ede78f6072e493b
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-05-26 04:20:07 -0300

    Update reset-username.ts

--- src/app/components/reset-username/reset-username.ts
+++ src/app/components/reset-username/reset-username.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule,Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../services/product';
+
 
 @Component({
   selector: 'app-reset-username',
@@ -7,5 +12,42 @@ import { Component } from '@angular/core';
   styleUrl: './reset-username.css',
 })
 export class ResetUsername {
+
+  resetForm: FormGroup;
+  token!: string;
+  mensajerror:string='';
+  successMessage:string='';
+ 
+  constructor(private fb: FormBuilder, private route: ActivatedRoute, private productService: Product) 
+  { // Crear el formulario
+  this.resetForm = this.fb.group({
+      newUsername['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnInit(): void {
+    // Leer el token de la URL
+    this.token = this.route.snapshot.queryParamMap.get('token') || '';
+
+    
+  }
+
+  onSubmit(): void {
+    if (this.resetForm.valid) {
+      const newUsername = this.resetForm.value.newUsername;
+      this.productService.resetUsername(this.token, newUsername({
+        next: (response) => {
+        // response.message viene del backend
+        alert(response.message); 
+        // También podés guardarlo en una variable para mostrarlo en el template
+        this.successMessage = response.message;
+      },
+        error: (err) =>{ 
+          alert('Error al actualizar contraseña: ' + err.message);
+        this.mensajerror=err.message;
+        }
+      });
+    }
+        }
 
 }
