277cf4a6f89ad686f966fdaa4c5c5a4e95d07c59
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-05-26 03:11:33 -0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { CartList} from './components/cart/cart';
 import { ForgotPassword } from './components/forgot-password/forgot-password';
 import { ForgotUsername } from './components/forgot-username/forgot-username';
 import { ResetPassword } from './components/reset-password/reset-password';
+import { ResetUsername } from './components/reset-username/reset-username';
 import { EstadoCompra } from './components/estado-compra/estado-compra';
 import { Registrarse } from './components/registrarse/registrarse';
 import { Dashboard } from './components/dashboard/dashboard';
@@ -18,6 +19,7 @@ export const routes: Routes = [{ path: '', component: ProductList},
  { path: 'forgot-password', component: ForgotPassword},      
                 { path: 'forgot-username', component: ForgotUsername},
   { path: 'reset-password', component: ResetPassword },
+                               { path: 'reset-username', component: ResetUsername },
             { path: 'dashboard', component: Dashboard },
 {path: 'login', component: Login },
  { path: 'estado-compra', component: EstadoCompra },
