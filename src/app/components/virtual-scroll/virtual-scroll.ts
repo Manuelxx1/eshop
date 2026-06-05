cb0ccfd030db9a1a7bb14d8690f2c16c5cbab676
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-06-05 08:14:17 -0300

    Implement OnInit to populate mock products
    Added OnInit lifecycle hook to initialize mock products array with 1000 items.

--- src/app/components/virtual-scroll/virtual-scroll.ts
+++ src/app/components/virtual-scroll/virtual-scroll.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
+interface MockProduct {
+  id: number;
+  name: string;
+  price: number;
+}
+
 
 @Component({
   selector: 'app-virtual-scroll',
-  imports: [],
+  imports: [CommonModule, ScrollingModule],
   templateUrl: './virtual-scroll.html',
   styleUrl: './virtual-scroll.css',
 })
-export class VirtualScroll {
+export class VirtualScroll implements OnInit{
+mockproducts: MockProduct[] = [];
 
+ngOnInit() {
+    this.mockproducts = Array.from({ length: 1000 }, (_, i) => ({
+      id: i + 1,
+      name: `Producto ${i + 1}`,
+      price: Math.floor(Math.random() * 1000)
+    }));
+}
+  
 }
