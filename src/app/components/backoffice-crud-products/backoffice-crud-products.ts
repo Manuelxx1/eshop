da47f0fe4dc0eeb26df837726cd800815d02fedc
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-06-10 10:24:12 -0300

    Define interfaces for product management
    Added interfaces for Section, Categories, and Product to define data structure.

--- src/app/components/backoffice-crud-products/backoffice-crud-products.ts
+++ src/app/components/backoffice-crud-products/backoffice-crud-products.ts
@@ -3,6 +3,30 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 import { Product } from '../../services/product';
 
+export interface Section {
+  id: number;
+  name: string;
+}
+
+export interface Categories {
+  id: number;
+  name: string;
+}
+
+export interface Product {
+  id?: number;   // opcional, lo genera la base
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  category: Categories;
+  section: Section; //  objeto Section completo
+  imageUrl: string;
+  createdAt?: Date;   //string o Date
+}
+
+
+
 
 @Component({
   selector: 'app-backoffice-crud-products',
