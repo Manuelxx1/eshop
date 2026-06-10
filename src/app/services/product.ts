5953d152eb0761b7f779ad89cf055973372479d0
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-06-10 10:35:58 -0300

    Make id and createdAt properties mandatory in Product

--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -23,7 +23,7 @@ export interface Categories {
 }
 
 export interface Product {
-  id?: number;   // opcional, lo genera la base
+  id: number;   // opcional, lo genera la base
   name: string;
   description: string;
   price: number;
@@ -31,7 +31,7 @@ export interface Product {
   category: Categories;
   section: Section; //  objeto Section completo
   imageUrl: string;
-  createdAt?: Date;   //string o Date
+  createdAt: Date;   //string o Date
 }
 
 export interface OrderItem {
