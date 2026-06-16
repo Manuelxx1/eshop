9b82c259e053aa83093896c1fd571f9e97f69f43
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-06-16 09:55:06 -0300

    Create CiberSeguridad2 component with template and styles

---
+++ src/app/components/articulo-noticias/tecnologia/ciberseguridad/ciberseguridad2.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ciberseguridad2',
+  imports: [],
+  templateUrl: './ciberseguridad2.html',
+  styleUrl: './ciberseguridad2.css',
+})
+export class CiberSeguridad2
+{
+
+}
