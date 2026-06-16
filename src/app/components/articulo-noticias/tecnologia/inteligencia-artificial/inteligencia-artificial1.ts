7a9120ead01edd28082877268f98d7e927c7dd49
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-06-16 07:44:58 -0300

    Create InteligenciaArtificial1 component

---
+++ src/app/components/articulo-noticias/tecnologia/inteligencia-artificial/inteligencia-artificial1.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inteligencia-artificial1',
+  imports: [],
+  templateUrl: './inteligencia-artificial1.html',
+  styleUrl: './inteligencia-artificial1.css',
+})
+export class InteligenciaArtificial1
+{
+
+}
