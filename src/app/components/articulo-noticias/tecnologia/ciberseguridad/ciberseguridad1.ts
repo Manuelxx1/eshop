afb37a1dd49638483421aca2d8e332ec97b40ffc
Luis Manuel Alegre <106556022+Manuelxx1@users.noreply.github.com>
2026-06-16 08:08:06 -0300

    Create CiberSeguridad1 component with template and styles

---
+++ src/app/components/articulo-noticias/tecnologia/ciberseguridad/ciberseguridad1.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ciberseguridad1',
+  imports: [],
+  templateUrl: './ciberseguridad1.html',
+  styleUrl: './ciberseguridad1.css',
+})
+export class CiberSeguridad1 
+{
+
+}
